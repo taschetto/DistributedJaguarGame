885a529acd2dd9e204e08d26b3db4a574ba704fe
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-13 01:13:17 -0300

    Create the board.

--- src/game/Board.java
+++ src/game/Board.java
@@ -5,15 +5,66 @@
  */
 package game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author 12180247
  */
 public class Board {
+
+  public static final int COLS = 7;
+  public static final int ROWS = 5;
   
-  public static final int X = 7;
-  public static final int Y = 5;
+  public static final int DOGS = 14;
   
-  Board() {
+  private Jaguar jaguar;
+  private Dog[] dogs;
+  
+  private final Map<Key, Position> positions;
+
+  public Board() {
+    jaguar = new Jaguar();
+
+    dogs = new Dog[Board.DOGS];
+    for (int i = 0; i < Board.DOGS; i++)
+      dogs[i] = new Dog();
+    
+    positions = new HashMap<>();
+    
+    for (int x = 0; x < Board.COLS; x++)
+      for (int y = 0; y < Board.ROWS; y++)
+        positions.put(new Key(x, y), new Position(x, y));
+    
+    positions.get(new Key(2, 2)).setPiece(jaguar);
+    
+    int i = 0;
+    for (int x = 0; x < 3; x++) {
+      for (int y = 0; y < Board.ROWS; y++) {
+        if (x == 2 && y == 2) continue;
+        positions.get(new Key(x, y)).setPiece(dogs[i++]);
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    String str = "";
+
+    for (int y = 0; y < Board.ROWS; y++) {
+      for (int x = 0; x < Board.COLS; x++) {
+        Piece piece = positions.get(new Key(x, y)).getPiece();
+        if (piece == null) {
+          if (x == 5 && (y == 0 || y == 4)) str += " ";
+          else str += "·";
+          continue;
+        }
+        
+        str += piece.toString();
+      }
+      str += "\n";
+    }
+    return str;
   }
 }
