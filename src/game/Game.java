c9b1fca64c4afb6948bc0a6b162bbc5430449a36
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-14 01:07:45 -0300

    Fix bug on Game.promptForDirection.

--- src/game/Game.java
+++ src/game/Game.java
@@ -14,11 +14,11 @@ public class Game {
   }
   
   public void play() {
-    //while(!board.hasWinner()) {
+    while(!board.hasWinner()) {
       System.out.println(board.toString());
       player1();
       //player2();
-    //}
+    }
   }
   
   public void player1() {
@@ -27,7 +27,6 @@ public class Game {
     Direction direction = null;
     while (!board.canMove(jaguar, direction = promptForDirection())) {
       System.out.println("Jaguar can't move " + direction.name() + "!");
-      direction = promptForDirection();
     }
     board.move(jaguar, direction);
   }
@@ -36,34 +35,33 @@ public class Game {
   }
   
   public Direction promptForDirection() {
-    Scanner in = new Scanner(System.in);
     StringBuilder output = new StringBuilder("Which way you wanna go?\n");
-    Direction[] directions = Direction.values();
 
+    Direction[] directions = Direction.values();
     for (int i = 0, len = directions.length; i < len; i++) {
       output.append("\t").append(i + 1).append(": ")
             .append(directions[i].name()).append("\n");
     }
     output.append(" >> ");
-    System.out.print(output.toString());
-    
+
     int playerChoice = 0;
-    while (playerChoice < 1 || playerChoice > 8) {
-      playerChoice = in.nextInt();
-    }
-    
-    in.close();
-    
     Direction dir = null;
-    switch (playerChoice) {
-      case 1: dir = Direction.Up;         break;
-      case 2: dir = Direction.UpRight;    break;
-      case 3: dir = Direction.Right;      break;
-      case 4: dir = Direction.DownRight;  break;
-      case 5: dir = Direction.Down;       break;
-      case 6: dir = Direction.DownLeft;   break;
-      case 7: dir = Direction.Left;       break;
-      case 8: dir = Direction.UpLeft;     break;
+
+    while (dir == null) {
+      System.out.print(output.toString());
+      playerChoice = Integer.parseInt(new Scanner(System.in).next());
+
+      switch (playerChoice) {
+        case 1:  dir = Direction.Up;        break;
+        case 2:  dir = Direction.UpRight;   break;
+        case 3:  dir = Direction.Right;     break;
+        case 4:  dir = Direction.DownRight; break;
+        case 5:  dir = Direction.Down;      break;
+        case 6:  dir = Direction.DownLeft;  break;
+        case 7:  dir = Direction.Left;      break;
+        case 8:  dir = Direction.UpLeft;    break;
+        default: dir = null;                break;
+      }
     }
 
     return dir;
