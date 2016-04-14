9c37baa9031520ede4e635ec4f17c19e60e0e69c
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-13 23:41:09 -0300

    Update Board.

--- src/game/Board.java
+++ src/game/Board.java
@@ -6,7 +6,11 @@
 package game;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -19,38 +23,65 @@ public class Board {
   public static final int DOGS = 14;
 
   private Jaguar jaguar;
-  private Dog[] dogs;
+  private Set<Dog> dogs;
 
   private final Map<Key, Position> positions;
 
   public Board() {
-    jaguar = new Jaguar();
-    dogs = new Dog[Board.DOGS];
+    jaguar = new Jaguar(this);
+    dogs = new HashSet<>();
 
     for (int i = 0; i < Board.DOGS; i++)
-      dogs[i] = new Dog();
+      dogs.add(new Dog(this));
 
     positions = new HashMap<>();
     for (int x = 0; x < Board.COLS; x++)
       for (int y = 0; y < Board.ROWS; y++)
         positions.put(new Key(x, y), new Position(x, y));
 
-    positions.get(new Key(2, 2)).setPiece(jaguar);
-
-    int i = 0;
-    for (int x = 0; x < 3; x++) {
-      for (int y = 0; y < Board.ROWS; y++) {
+    positions.get(new Key(3, 0)).setPiece(jaguar);
+    
+    Iterator<Dog> it = dogs.iterator();
+    for (int x = 0; it.hasNext() && x < 3; x++) {
+      for (int y = 0; it.hasNext() && y < Board.ROWS; y++) {
         if (x == 2 && y == 2) continue;
-        positions.get(new Key(x, y)).setPiece(dogs[i++]);
+        positions.get(new Key(x, y)).setPiece(it.next());
       }
     }
+  }
+  
+  public boolean canMove(Piece piece, Direction direction) {
+    return piece.canMove(direction);
+  }
+  
+  public void move(Piece piece, Direction direction) {
+    piece.move(direction);
+  }
+  
+  public Jaguar getJaguar() {
+    return this.jaguar;
+  }
+  
+  public boolean hasWinner() {
+    return verifyDogsVictory() || verifyJaguarVictory();
+  }
+  
+  public boolean verifyDogsVictory() {
+    // Para os cães ganharem o jogo, a onça precisa não ter nenhum movimento
+    // válido à partir da sua posição atual.
+    return false;
+  }
+  
+  public boolean verifyJaguarVictory() {
+    // Para a onça ganhar o jogo, basta ter comido 5 cães.
+    return dogs.size() < 10;
   }
 
   @Override
   public String toString() {
     String str = "";
 
-    for (int y = 0; y < Board.ROWS; y++) {
+    for (int y = Board.ROWS - 1; y >= 0; y--) {
       for (int x = 0; x < Board.COLS; x++) {
         Piece piece = positions.get(new Key(x, y)).getPiece();
         if (piece == null) {
@@ -58,7 +89,7 @@ public class Board {
           else str += "·";
           continue;
         }
-        
+
         str += piece.toString();
       }
       str += "\n";
