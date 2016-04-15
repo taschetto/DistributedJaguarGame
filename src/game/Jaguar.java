c621bdac6e17e3d89a98f9776383afc4e6fc448e
Guilherme Taschetto <gtaschetto@gmail.com>
2016-04-14 21:32:20 -0300

    Add comments.

--- src/game/Jaguar.java
+++ src/game/Jaguar.java
@@ -15,25 +15,33 @@ public class Jaguar extends Piece {
   {
     if (direction == null) return false;
     
+    // Pega as coordenadas atuais do Jaguar.
     int x = this.position.getX(),
         y = this.position.getY();
     
+    // Valida se a direção é válida.
     MoveValidator validator = MoveValidator.getInstance();
     if (!validator.validate(x, y, direction))
       return false;
     
+    // Se a direção é válida, valida se a nova posição está disponível.
     Position nextPosition = this.board.getPosition(direction.getNextPosition(x, y));
     return nextPosition.getPiece() == null;
   }
 
   @Override
   public Key move(Direction direction) {
+    // Libera a posição atual.
     this.getPosition().setPiece(null);
 
+    // Calcula a chave da nova posição indo na direção.
     MoveValidator validator = MoveValidator.getInstance();
     Key nextKey = direction.getNextPosition(this.position.getX(), this.position.getY());
     
+    // Busca a nova posição usando a chave calculada.
     Position position = this.board.getPosition(nextKey);
+    
+    // Coloca a peça na nova posição, de fato.
     position.setPiece(this);
 
     return nextKey;
