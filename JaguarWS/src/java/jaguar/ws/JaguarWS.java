2d2544ce6a0f1168b53a58b720aa942f3f6f3146
Guilherme Taschetto <gtaschetto@gmail.com>
2016-06-07 22:20:30 -0300

    Call manager.

--- JaguarWS/src/java/jaguar/ws/JaguarWS.java
+++ JaguarWS/src/java/jaguar/ws/JaguarWS.java
@@ -5,6 +5,9 @@
  */
 package jaguar.ws;
 
+import jaguar.common.Direction;
+import jaguar.server.server.JaguarGame;
+import jaguar.server.util.Manager;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -15,6 +18,15 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "JaguarWS")
 public class JaguarWS {
+  
+  public static final int MAX_GAMES = 50;
+  public static final int MAX_PLAYERS = MAX_GAMES * 2;
+  
+  private final Manager manager;
+  
+  public JaguarWS() {
+    this.manager = new Manager(JaguarGame.MAX_PLAYERS);
+  }
 
   /**
    * Web service operation
@@ -29,71 +41,63 @@ public class JaguarWS {
    * Web service operation
    */
   @WebMethod(operationName = "registraJogador")
-  public int registraJogador(@WebParam(name = "playerName") final String playerName) {
-    //TODO write your implementation code here:
-    return 0;
+  public int registraJogador(@WebParam(name = "playerName") final String playerName) throws InterruptedException {
+    return this.manager.registerPlayer(playerName);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "encerraPartida")
-  public int encerraPartida(@WebParam(name = "playerId") final int playerId) {
-    //TODO write your implementation code here:
-    return 0;
+  public int encerraPartida(@WebParam(name = "playerId") final int playerId) throws InterruptedException {
+    return this.manager.endGame(playerId);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "temPartida")
-  public int temPartida(@WebParam(name = "playerId") final int playerId) {
-    //TODO write your implementation code here:
-    return 0;
+  public int temPartida(@WebParam(name = "playerId") final int playerId) throws InterruptedException {
+    return this.manager.hasGame(playerId);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "ehMinhaVez")
-  public int ehMinhaVez(@WebParam(name = "playerId") final int playerId) {
-    //TODO write your implementation code here:
-    return 0;
+  public int ehMinhaVez(@WebParam(name = "playerId") final int playerId) throws InterruptedException {
+    return this.manager.isMyTurn(playerId);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "obtemGrade")
-  public String obtemGrade(@WebParam(name = "playerId") final int playerId) {
-    //TODO write your implementation code here:
-    return null;
+  public String obtemGrade(@WebParam(name = "playerId") final int playerId) throws InterruptedException {
+    return this.manager.getGrid(playerId);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "enviaJogadaOnca")
-  public int enviaJogadaOnca(@WebParam(name = "playerId") final int playerId, @WebParam(name = "jaguarDirection") final int jaguarDirection) {
-    //TODO write your implementation code here:
-    return 0;
+  public int enviaJogadaOnca(@WebParam(name = "playerId") final int playerId, @WebParam(name = "direction") final int direction) throws InterruptedException {
+    return this.manager.sendMove(playerId, -1, Direction.values()[direction]);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "enviaJogadaCao")
-  public int enviaJogadaCao(@WebParam(name = "playerId") final int playerId, @WebParam(name = "dogId") final int dogId, @WebParam(name = "dogDirection") final int dogDirection) {
-    //TODO write your implementation code here:
-    return 0;
+  public int enviaJogadaCao(@WebParam(name = "playerId") final int playerId, @WebParam(name = "dogId") final int dogId, @WebParam(name = "direction") final int direction) throws InterruptedException {
+    return this.manager.sendMove(playerId, dogId, Direction.values()[direction]);
   }
 
   /**
    * Web service operation
    */
   @WebMethod(operationName = "obtemOponente")
-  public String obtemOponente(@WebParam(name = "playerId") final int playerId) {
-    //TODO write your implementation code here:
-    return null;
+  public String obtemOponente(@WebParam(name = "playerId") final int playerId) throws InterruptedException {
+    return this.manager.getOpponent(playerId);
   }
 }
